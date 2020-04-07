9cb09639db1a40da9012a5b004d75e7f487f32ae
JacobCXDev <jacobcxdev@gmail.com>
2020-04-07 06:31:38 +0100

    Fixed preferences not persisting

--- fbspnosorpreferences/FBPRootListController.m
+++ fbspnosorpreferences/FBPRootListController.m
@@ -11,12 +11,12 @@
 }
 - (id)readPreferenceValue:(PSSpecifier*)specifier {
     NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
-    NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    NSMutableDictionary *plist = [NSMutableDictionary dictionaryWithContentsOfFile:path];
 	return (plist[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
 }
 - (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
     NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
-    NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    NSMutableDictionary *plist = [NSMutableDictionary dictionaryWithContentsOfFile:path] ?: [NSMutableDictionary new];
 	plist[specifier.properties[@"key"]] = value;
 	[plist writeToFile:path atomically:true];
 	CFStringRef notificationName = (__bridge CFStringRef)specifier.properties[@"PostNotification"];
